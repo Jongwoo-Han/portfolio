bed205b7cfa5d27f95cd40a70d0bf1f76ddd014c
jongwooo <han980817@gmail.com>
2020-03-09 01:53:40 +0900

    Create interface.ts

---
+++ src/interface.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import { ReplaceComponentRendererArgs } from "gatsby"
+
+export type ITemplateProps<T> = ReplaceComponentRendererArgs["props"] & {
+  pageContext: {
+    isCreatedByStatefulCreatePages: boolean
+  } & T
+}
