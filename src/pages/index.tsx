0de76e5ff60f5eb24d5b0e8b689883dce339e752
jongwooo <han980817@gmail.com>
2020-03-09 01:52:12 +0900

    Update pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,66 @@
-/* eslint-disable no-unused-vars */
-import * as React from "react";
-import { Link } from "gatsby";
+import * as React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
-import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "./index.css"
+import { Query } from "../../types/graphql-types"
 
-const IndexPage: React.FC = () => (
-	<Layout>
-		<SEO title="Home" />
-		<h1>Hi people and TypeScript!</h1>
-		<p>Welcome to your new Gatsby site.</p>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
-		<Link to="/page-2/">Go to page 2</Link>
-	</Layout>
-);
+const LatestPostListQuery = graphql`
+  query LatestPostListQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          excerpt(truncate: true, pruneLength: 200)
+          frontmatter {
+            title
+            path
+            date(formatString: "MMM D")
+            author
+          }
+          id
+        }
+      }
+    }
+  }
+`
 
-export default IndexPage;
+const IndexPage: React.FC = () => {
+  const data = useStaticQuery<Query>(LatestPostListQuery)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <ul className="content-ul">
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <p className="content-log content-item">
+              <strong>{node.frontmatter.author}</strong> created a post{" "}
+              <strong>
+                <Link to={node.frontmatter.path} className="content-link">
+                  {node.frontmatter.title}
+                </Link>
+              </strong>{" "}
+              at {node.frontmatter.date}
+            </p>
+            <li className="content-li">
+              <h4 className="content-title">
+                <Link to={node.frontmatter.path} className="content-link">
+                  {node.frontmatter.title}
+                </Link>
+              </h4>
+              <p className="content-item">{node.excerpt}</p>
+              <small className="content-item">
+                {" "}
+                Updated {node.frontmatter.date}
+              </small>
+            </li>
+            <hr color="#d1d5da" className="content-hr" />
+          </div>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+export default IndexPage
