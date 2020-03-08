8d67b08682c28f56d826f0a54a18f574c0cc1876
jongwooo <han980817@gmail.com>
2020-03-09 01:52:45 +0900

    Create createPages.ts in lib

---
+++ src/lib/createPages.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { CreatePagesArgs } from "gatsby"
+import path from "path"
+import { Query } from "../../types/graphql-types"
+
+export async function createPages({ actions, graphql }: CreatePagesArgs) {
+  const { createPage } = actions
+
+  const { data, errors } = await graphql<Query>(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            frontmatter {
+              title
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (errors) {
+    throw errors
+  }
+  
+  data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: String(node.frontmatter.path),
+      context: {
+        html: node.html,
+        title: String(node.frontmatter.title),
+      },
+      component: path.resolve(__dirname, "../templates/PostTemplate.tsx"),
+    })
+  })
+}
